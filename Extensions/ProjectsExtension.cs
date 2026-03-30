a0472c0e3ffe5bdd525c34acf02bd4a091f2234f
robhowe-A <rhowell91@live.com>
2026-03-30 08:45:12 -0500

    Project extension method;

---
+++ Extensions/ProjectsExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ProjectsPage.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication UseProjectSecurityHeaders(this WebApplication app)
+    {
+        app.Use((context, next) =>
+        {
+            context.Response.Headers.Append(
+                "Content-Security-Policy",
+                "upgrade-insecure-requests; default-src 'self' wss:; img-src 'self' data:; connect-src https: wss:; frame-ancestors 'self'; frame-src 'self' *; script-src 'self' https://static.cloudflareinsights.com 'sha256-f97Y2Dpz08nctjpmlolnUHCpzBPzgBQNuU9k3U8rCJg=' 'sha256-TlR0wK5VhVNKCRNCjZDLXK4LmS2rxHXS+M6ooMqO//c=' 'sha256-PWJ72QSVNqhMJQvmMDLk9r4IylnqHkLPiWnSe0qVn8k='");
+
+            context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=()");
+            context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
+            context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Append("X-Frame-Options", "DENY");
+
+            return next(context);
+        });
+
+        return app;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ProjectsPage.Components;
 using ProjectsPage.Models;
 using ProjectsPage.Services;
+using ProjectsPage.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,7 +35,6 @@ builder.Services.AddHsts(options =>
     options.Preload = true;
 });
 
-
 builder.Services.AddMvc(options =>
 {
     options.EnableEndpointRouting = false;
@@ -54,16 +54,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
-app.Use((context, next)  =>
-{
-    context.Response.Headers.Append("Content-Security-Policy", "upgrade-insecure-requests; default-src 'self' wss:; img-src 'self' data:; connect-src https: wss:; frame-ancestors 'self'; frame-src 'self' *; script-src 'self' https://static.cloudflareinsights.com 'sha256-f97Y2Dpz08nctjpmlolnUHCpzBPzgBQNuU9k3U8rCJg=' 'sha256-TlR0wK5VhVNKCRNCjZDLXK4LmS2rxHXS+M6ooMqO//c=' 'sha256-PWJ72QSVNqhMJQvmMDLk9r4IylnqHkLPiWnSe0qVn8k='");
-    context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=()");
-    context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
-    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
-    context.Response.Headers.Append("X-Frame-Options", "DENY");
-    
-    return next(context);
-});
+app.UseProjectSecurityHeaders();
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
