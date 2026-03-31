fe8f1c0a97875f70f76ecf153da2a5dc0244fae7
robhowe-A <rhowell91@live.com>
2026-03-31 09:11:29 -0500

    sidebar styles change

--- Components/SelectedWebsiteDetails.razor.cs
+++ Components/SelectedWebsiteDetails.razor.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace ProjectsPage.Components.Pages;
+namespace ProjectsPage.Components;
 
 public partial class SelectedWebsiteDetails : ComponentBase
 {
