d079efdb076a15709099652c64ecffa73f653398
robhowe-A <rhowell91@live.com>
2026-04-01 11:10:28 -0500

    file structure change;

---
+++ Projects-Unit-Tests/UnitTests/EntityModelTests.cs
@@ -0,0 +1,14 @@
+﻿using ProjectsPage.Models;
+
+namespace UnitTests
+{
+    public class EntityModelTests
+    {
+        [Fact]
+        public void JsonDocStoreConnectTest()
+        {
+            using ProjectsDbContext db = EntityModels.CreateProjectsDbContext();
+            Assert.True(db.Database.CanConnect());
+        }
+    }
+}
