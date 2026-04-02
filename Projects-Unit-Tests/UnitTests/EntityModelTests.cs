64ef5099a41e0c8a83f027cd8a1cd49d0ec751b3
robhowe-A <rhowell91@live.com>
2026-04-02 15:20:31 -0500

    Facts update;

--- Projects-Unit-Tests/UnitTests/EntityModelTests.cs
+++ Projects-Unit-Tests/UnitTests/EntityModelTests.cs
@@ -1,4 +1,6 @@
-﻿using ProjectsPage.Models;
+﻿using ProjectsPage.Components;
+using ProjectsPage.Models;
+using System.Text.Json;
 
 namespace UnitTests
 {
@@ -8,7 +10,39 @@ namespace UnitTests
         public void JsonDocStoreConnectTest()
         {
             using ProjectsDbContext db = EntityModels.CreateProjectsDbContext();
-            Assert.True(db.Database.CanConnect());
+            Assert.True( db.Database.CanConnect() );
+        }
+
+        [Fact]
+        public void JsonDocSoreCountConfirmation()
+        {
+            using ProjectsDbContext db = EntityModels.CreateProjectsDbContext();
+            var count = (from project in db.projects where project.site == "ProjectsPage" select project.site).Count();
+            count += (from project in db.projects where project.site == "ProjectsPageDemos" select project.site).Count();
+            db.Dispose();
+
+            Assert.True( count == 12 );
+        }
+
+        [Fact]
+        public void CheckAgileStockNameMatchInDatase()
+        {
+            using ProjectsDbContext db = EntityModels.CreateProjectsDbContext();
+            
+            //var DocumentTitles = db.projects.FromSqlRaw("""select document->"$.Title" from projects where site='ProjectsPage';"""); //Not an OOM: no array or list method
+            var AgileStockDocument = (from project in db.projects where project.site == "ProjectsPage" && project.document.Contains("AgileStock Web") select project.document).ToList();
+            db.Dispose();
+
+            if (AgileStockDocument.Count < 1) throw new NullReferenceException();
+
+            JsonSerializerOptions options = new JsonSerializerOptions(new JsonSerializerDefaults());
+            options.PropertyNameCaseInsensitive = false;
+
+            FrameSelectionOption jdocAgileStock = JsonSerializer.Deserialize<FrameSelectionOption>
+                (AgileStockDocument.First(), options)
+                ?? throw new InvalidDataException("Encountered a document serialization error.");
+
+            Assert.Equal( "AgileStock Web", jdocAgileStock.Title.ToString() );
         }
     }
-}
+};
