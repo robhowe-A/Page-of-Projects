512531019b1de8201249ff7f6bd8d2e9d0cd250b
Robert Howell <rhowell91@live.com>
2026-06-21 19:45:30 -0500

    Component abstraction addition;

---
+++ Page-of-Projects/Presentation/Aside/SortSelectionsBase.cs
@@ -0,0 +1,26 @@
+﻿// --Copyright (c) 2026 Robert A. Howell
+
+using Microsoft.AspNetCore.Components;
+using ProjectsPage.Domain;
+
+namespace ProjectsPage.Components.Secondary;
+
+public abstract class SortSelectionsBase : ComponentBase
+{
+    private SortOrder SortOrderSelected { get; set; }
+
+    [Parameter]
+    public required string DeploymentAs { get; set; }
+
+    [Parameter]
+    public EventCallback<(SortOrder, string)> OnResortedListSelected { get; set; }
+
+    [Parameter]
+    public required List<FrameSelectionOption> DemosSelections { get; set; }
+
+    protected async Task InvokeSortAsync(SortOrder sortOrder)
+    {
+        SortOrderSelected = sortOrder;
+        await OnResortedListSelected.InvokeAsync((SortOrderSelected, DeploymentAs));
+    }
+};
