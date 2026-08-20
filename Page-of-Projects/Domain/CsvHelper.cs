ca68ef4f31e291b2c991116920b80e917f3909b9
Robert Howell <rhowell376@outlook.com>
2026-08-20 09:10:33 -0500

    Csv export addition; Link tag interactivity;
    Signed-off-by: Robert Howell <rhowell376@outlook.com>

---
+++ Page-of-Projects/Domain/CsvHelper.cs
@@ -0,0 +1,28 @@
+﻿// --Copyright (c) 2026 Robert A. Howell
+
+using System.Globalization;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace ProjectsPage.Domain;
+
+public static class CsvExportHelper
+{
+    public static byte[] ToCsvBytes<T>(IEnumerable<T> records, bool includeHeader = true)
+    {
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, new UTF8Encoding(encoderShouldEmitUTF8Identifier: true), leaveOpen: true);
+        using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
+          {
+                  HasHeaderRecord = includeHeader,
+                  ShouldQuote = args => true
+          });
+
+        csv.WriteRecords(records);
+        writer.Flush();
+        writer.Close();
+
+        return memoryStream.ToArray();
+    }
+};
